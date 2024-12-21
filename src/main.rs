f3db4f3e66febfe1bc3843fddedd5451f8ec485c
vic <nytalvi@gmail.com>
2024-12-21 17:49:43 -0500

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ping::*;
 use std::net::IpAddr;
 use colored::*;
-use std::time::Duration;
+use tokio::time::{self, Duration};
 
 fn justping(ip: IpAddr) {
     let timeout = Duration::from_secs(1); // Set timeout to 1 second
@@ -17,9 +17,11 @@ fn justping(ip: IpAddr) {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let ipfile: String;
+    let interval = Duration::from_secs(1);
 
     if args.len() == 2 {
         ipfile = std::fs::read_to_string(&args[1]).expect("Failed to read file");
