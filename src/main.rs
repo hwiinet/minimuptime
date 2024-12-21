73a1fe7656b5fae1b5281066b7a52e9de07d23da
vic <nytalvi@gmail.com>
2024-12-21 17:35:29 -0500

    read from iplist.txt, or specified arg file

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,22 @@ fn justping(ip: IpAddr) {
 }
 
 fn main() {
-    // collect args and parse them
-    let args: Vec<String> = std::env::args().collect();
-    let ip = args[1].parse().unwrap();
-    if args.len() < 2 {
-        println!("Usage: {} <ip>", args[0]);
-        return;
+    let args = std::env::args().collect::<Vec<String>>();
+    let ipfile: String;
+
+    if args.len() == 2 {
+        ipfile = std::fs::read_to_string(&args[1]).expect("Failed to read file");
+    } else {
+        ipfile = std::fs::read_to_string("iplist.txt").expect("Failed to read iplist.txt");
     }
-   
-   justping(ip); 
+
+    let iplist: Vec<&str> = ipfile.split("\n").collect();
+
+    for ip in iplist {
+        if ip.len() > 0 {
+            let ip = ip.parse().expect("Failed to parse IP address");
+            justping(ip);
+        }
+    }
 }
 
