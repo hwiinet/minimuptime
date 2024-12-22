a6a0abc3f952c14a35366859e2225c95384954f2
vic <nytalvi@gmail.com>
2024-12-22 13:54:12 -0500

    read from config file

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use colored::*;
 use std::thread;
 use std::time::Duration;
 use std::io::Write;
+use serde::Deserialize;
 
-fn justping(ip: IpAddr) {
-    let timeout = Duration::from_secs(1); // Set timeout to 1 second
+#[derive(Deserialize)]
+struct MmuConfig {
+    interval: u64,
+    ipfile: String,
+    timeout: i64,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    mmu: MmuConfig,
+}
+
+fn justping(ip: IpAddr, conftimeout: u64) {
+    let timeout = Duration::from_secs(conftimeout); // Set timeout to 1 second
     let ping_result = ping(ip, Some(timeout), None, None, None, None);
     let logfile = "log.txt";
 
@@ -28,16 +41,18 @@ fn justping(ip: IpAddr) {
 
 #[tokio::main]
 async fn main() {
+    let configfile = std::fs::read_to_string("minimuptime.toml").expect("Failed to read minimuptime.toml");
+    let config = toml::from_str::<Config>(&configfile).expect("Failed to parse minimuptime.toml");
     let args = std::env::args().collect::<Vec<String>>();
-    let ipfile: String;
-    let interval = Duration::from_secs(300);
+    let mut ipfile = std::fs::read_to_string(&config.mmu.ipfile).expect("Failed to read ipfile");
+    let interval = Duration::from_secs(config.mmu.interval);
+    let timeout: i64 = config.mmu.timeout;
 
     if args.len() == 2 {
         ipfile = std::fs::read_to_string(&args[1]).expect("Failed to read file");
-        println!("[NOTICE] {}", "Reading IP list from file...".cyan().bold());
+        println!("[NOTICE] {} {}...", "Reading IP list from file".cyan().bold(), args[1].cyan().bold());
     } else {
-        ipfile = std::fs::read_to_string("iplist.txt").expect("Failed to read iplist.txt");
-        println!("[NOTICE] {}", "Reading IP list from iplist.txt...".cyan().bold());
+        println!("[NOTICE] {}", "Reading IP list defined in config file...".cyan().bold());
     }
 
     let iplist: Vec<&str> = ipfile.split("\n").collect();
@@ -47,7 +62,7 @@ async fn main() {
         for ip in &iplist {
             if ip.len() > 0 {
                 let ip = ip.parse().expect("Failed to parse IP address");
-                justping(ip);
+                justping(ip, timeout.try_into().unwrap());
             }
         }
         thread::sleep(interval);
